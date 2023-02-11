81edb995251c4202f874207cf069a1cece7a7208
ilham88 <iam.klopers@gmail.com>
2023-02-11 08:37:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/lazygramyt/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
-python3 main.py
+git clone https://github.com/ilham88/CloneKlop ok && cd ok && pip3 install -U -r requirements.txt
+bash run.sh
